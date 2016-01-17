651bd7dde40c304f1e5e48d6df84c424ba1617e3
Victor Grigoriu <vgrigoriu@gmail.com>
2016-01-17 16:33:01 +0200

    Add some FxCop rules as errors

--- Spreadsheet/Sheet.cs
+++ Spreadsheet/Sheet.cs
@@ -2,6 +2,8 @@
 {
     public class Sheet
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "cellAddress")]
         public string Get(string cellAddress)
         {
             return string.Empty;
