406733cdbcecb26b3f28b6af4257a6d17ecbe836
Victor Grigoriu <vgrigoriu@gmail.com>
2016-01-17 21:53:00 +0200

    Introduce private Cell class
    and failing test about numeric values

--- Spreadsheet.Tests/Part1Tests.cs
+++ Spreadsheet.Tests/Part1Tests.cs
@@ -2,6 +2,7 @@
 
 namespace Spreadsheet.Tests
 {
+    [TestFixture]
     public class Part1Tests
     {
         //public void testThatCellsAreEmptyByDefault() {
@@ -9,6 +10,7 @@ namespace Spreadsheet.Tests
         //  assertEquals("", sheet.get("A1"));
         //  assertEquals("", sheet.get("ZX347"));
         //}
+        [Test]
         public void CellsAreEmptyByDefault()
         {
             var sheet = new Sheet();
@@ -30,6 +32,7 @@ namespace Spreadsheet.Tests
         //    sheet.put(theCell, "");
         //    assertEquals("", sheet.get(theCell));
         //}
+        [Test]
         public void TextCellsAreStored()
         {
             var sheet = new Sheet();
@@ -54,6 +57,7 @@ namespace Spreadsheet.Tests
         //    assertEquals("X27 same", "Second", sheet.get("X27"));
         //    assertEquals("ZX901 same", "Third", sheet.get("ZX901"));
         //}
+        [Test]
         public void ManyCellsExist()
         {
             var sheet = new Sheet();
@@ -62,6 +66,45 @@ namespace Spreadsheet.Tests
 
             Assert.That(sheet.Get("A1"), Is.EqualTo("First"));
             Assert.That(sheet.Get("X27"), Is.EqualTo("Second"));
+        }
+
+        //public void testThatNumericCellsAreIdentifiedAndStored()
+        //{
+        //    Sheet sheet = new Sheet();
+        //    String theCell = "A21";
+
+        //    sheet.put(theCell, "X99"); // "Obvious" string
+        //    assertEquals("X99", sheet.get(theCell));
+
+        //    sheet.put(theCell, "14"); // "Obvious" number
+        //    assertEquals("14", sheet.get(theCell));
+
+        //    sheet.put(theCell, " 99 X"); // Whole string must be numeric
+        //    assertEquals(" 99 X", sheet.get(theCell));
+
+        //    sheet.put(theCell, " 1234 "); // Blanks ignored
+        //    assertEquals("1234", sheet.get(theCell));
+
+        //    sheet.put(theCell, " "); // Just a blank
+        //    assertEquals(" ", sheet.get(theCell));
+        //}
+        [Test]
+        public void NumericCellsAreIdentifiedAndStored()
+        {
+            var sheet = new Sheet();
+            const string theCell = "B14";
+
+            sheet.Put(theCell, "14");
+            Assert.That(sheet.Get(theCell), Is.EqualTo("14"));
+
+            sheet.Put(theCell, " 99 X");
+            Assert.That(sheet.Get(theCell), Is.EqualTo(" 99 X"));
+
+            sheet.Put(theCell, " 1234 ");
+            Assert.That(sheet.Get(theCell), Is.EqualTo("1234"));
+
+            sheet.Put(theCell, " ");
+            Assert.That(sheet.Get(theCell), Is.EqualTo(" "));
         }
     }
 }
--- Spreadsheet/Sheet.cs
+++ Spreadsheet/Sheet.cs
@@ -4,18 +4,30 @@ namespace Spreadsheet
 {
     public class Sheet
     {
-        private readonly Dictionary<string, string> cells = new Dictionary<string, string>();
+        private readonly Dictionary<string, Cell> cells = new Dictionary<string, Cell>();
 
         public string Get(string cellAddress)
         {
             return cells.ContainsKey(cellAddress) 
-                ? cells[cellAddress] 
+                ? cells[cellAddress].Value
                 : string.Empty;
         }
 
         public void Put(string cellAddress, string content)
         {
-            cells[cellAddress] = content;
+            cells[cellAddress] = new Cell(content);
+        }
+
+        private class Cell
+        {
+            private readonly string rawContent;
+
+            public string Value => rawContent;
+
+            public Cell(string rawContent)
+            {
+                this.rawContent = rawContent;
+            }
         }
     }
 }
