d78a6c19b36a8c06b228b44231a985c56aa53966
Victor Grigoriu <vgrigoriu@gmail.com>
2016-01-17 16:13:36 +0200

    Add fixie, nunit and one test

---
+++ Spreadsheet.Tests/CanRunTest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace Spreadsheet.Tests
+{
+    public class CanRunTests
+    {
+        public void CanRunTest()
+        {
+            Assert.That(1 + 1, Is.EqualTo(2));
+        }
+    }
+}
